48a9898ec2f8fb04a02ba3ac7c2cf555ddee2bd2
jchereck <josephchereck@gmail.com>
2019-02-07 13:24:44 -0800

    implemented rough reading of training data, splitting to X and Y, then normalization

--- nn.cpp
+++ nn.cpp
@@ -3,21 +3,53 @@
 
 int main(){
 
-	Matrix inpt("New Array");
+	int numIn, readStatus;
+	readStatus = scanf("%d", &numIn);
 
-	inpt.read();
-	inpt.print();
-	inpt.read();
-	inpt.print();
-	inpt.read();
-	inpt.print();
-	inpt.read();
-	inpt.print();
-	inpt.read();
-	inpt.print();
-	inpt.read();
-	inpt.print();
+	if( readStatus == 0 ){
+		printf("Error Reading Input\n");
+		exit(-1);
+	}
+	
+	printf("Inputs: %d\n", numIn);
+	
+	//Read in training matrix data
+	Matrix mIn("Raw Training Data");
+	mIn.read();
 
+	mIn.print();
+
+	//Split raw input into X and T matricies
+	printf("Splitting Training Data\n");
+
+	Matrix mX = mIn.extract(0, 0, 0, numIn);
+	Matrix mT = mIn.extract(0, numIn, 0, 0);
+
+	mX.print();
+	mT.print();
+
+	//Normalize mX
+	printf("mX min %f and max %f\n", mX.min(), mX.max());
+
+	double mXcolMin[mX.numCols()];
+	double mXcolMax[mX.numCols()];
+
+	for( int i = 0; i < mX.numCols(); i++ ){
+		printf("min: %f max: %f\n", mX.minCol(i), mX.maxCol(i));
+		mXcolMin[i] = mX.minCol(i);
+		mXcolMax[i] = mX.maxCol(i);
+	}
+
+	for( int i = 0; i < mX.numCols(); i++ ){
+		printf("min: %f max: %f\n", mXcolMin[i], mXcolMax[i]);
+	}
+	
+	mX.normalizeCols();
+
+	mX.print();
+	
+	
+	
 	return 0;
 }
 
